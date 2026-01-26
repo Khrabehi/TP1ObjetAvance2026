f0bcd270ab62f5e6af4b8a6e71e4e18f5fe0dc85
Khrabehi <khalisrabehi@gmail.com>
2026-01-26 09:55:54 +0100

    dont use variable acces with method direclty

--- src/tp1_cpp/Arbre.cpp
+++ src/tp1_cpp/Arbre.cpp
@@ -2,6 +2,5 @@
 #include "Arbre.hpp"
 
 void Arbre::croissance() {
-    float currentTaille = getTaille();
-    setTaille(currentTaille + 1.0f);
+    setTaille(getTaille() + 1.0f);
 }
--- src/tp1_cpp/Fleur.cpp
+++ src/tp1_cpp/Fleur.cpp
@@ -4,9 +4,8 @@
 Fleur::Fleur(float tailleMax): mTailleMax(tailleMax) {}
 
 void Fleur::croissance() {
-    float currentTaille = getTaille();
-    if (currentTaille + 0.1f <= mTailleMax) {
-        setTaille(currentTaille + 0.1f);
+    if (getTaille() + 0.1f <= mTailleMax) {
+        setTaille(getTaille() + 0.1f);
     } else {
         setTaille(mTailleMax);
     }
