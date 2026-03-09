928b16e5bb9859eb391dab16372ebea3fcc5c13d
Khrabehi <khalisrabehi@gmail.com>
2026-03-09 09:37:06 +0100

    add adapter class

---
+++ src/tp1_cpp/adapter/ChampignonAdapter.cpp
@@ -0,0 +1,26 @@
+#include "ChampignonAdapter.hpp"
+
+ChampignonAdapter::ChampignonAdapter(std::unique_ptr<Champignon> vraiChampi)
+    : champignon(std::move(vraiChampi))
+{
+
+}
+
+ChampignonAdapter::~ChampignonAdapter() = default;
+
+double ChampignonAdapter::getTaille() const {
+    return champignon->getHauteurChampignon();
+}
+
+void ChampignonAdapter::croissance() {
+    champignon->pousse(Champignon::Meteo::Soleil);
+    setTaille(champignon->getHauteurChampignon());
+}
+
+std::string ChampignonAdapter::getType() const {
+    return "Champignon";
+}
+
+void ChampignonAdapter::setType(double taille) {
+    setTaille(taille);
+}
---
+++ src/tp1_cpp/adapter/ChampignonAdapter.hpp
@@ -0,0 +1,21 @@
+#include "Vegetal.hpp"
+#include "Champignon.hpp"
+#include <memory>
+
+class ChampignonAdapter : public Vegetal {
+private: 
+    std::unique_ptr<Champignon> champignon;
+
+public: 
+    ChampignonAdapter(std::unique_ptr<Champignon> vraiChampi);
+    ~ChampignonAdapter();
+
+    double getTaille() const;
+
+    void croissance(); 
+    std::string getType() const;
+
+protected:
+    void setType(double taille);
+
+};
